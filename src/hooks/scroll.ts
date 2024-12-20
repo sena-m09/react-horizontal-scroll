c4d6aab9f19861eacb13443636fe4c0d03ca168e
sena-m09 <sena-murakami@gaji.jp>
2024-12-20 15:34:31 +0900

    fix: リファクタ

--- src/hooks/scroll.ts
+++ src/hooks/scroll.ts
@@ -2,8 +2,7 @@ import { useEffect, useState, useCallback } from "react";
 
 import { throttle } from "throttle-debounce";
 
-export interface HorizontalScrollState {
-  scrollLeft: number;
+type HorizontalScrollState = {
   canScrollLeft: boolean;
   canScrollRight: boolean;
 }
@@ -16,39 +15,30 @@ export const useHorizontalScrollState = <
   scrollBarHeight: number;
 } => {
   const [element, setElement] = useState<T | null>(null);
-
-  const refCallback = useCallback((node: T | null) => {
-    if (node) {
-      setElement(node);
-    }
-  }, []);
-
-  const [scrollBarHeight, setScrollBarHeight] = useState(() => {
-    return element
-      ? (element.offsetHeight ?? 0) - (element.clientHeight ?? 0)
-      : 0;
-  });
+  const [scrollBarHeight, setScrollBarHeight] = useState(0);
   const [state, setState] = useState<HorizontalScrollState>({
-    scrollLeft: 0,
     canScrollLeft: false,
     canScrollRight: false,
   });
 
   const updateScrollBarHeight = useCallback(() => {
-    setScrollBarHeight(
-      (element?.offsetHeight ?? 0) - (element?.clientHeight ?? 0)
-    );
+    if (!element) return;
+    setScrollBarHeight(element.offsetHeight - element.clientHeight);
   }, [element]);
 
   const updateScrollState = useCallback(() => {
     if (!element) return;
     const { scrollLeft, clientWidth, scrollWidth } = element;
     setState({
-      scrollLeft,
       canScrollLeft: scrollLeft > 0,
       canScrollRight: scrollLeft + clientWidth < scrollWidth,
     });
   }, [element]);
+
+  const handleScroll = useCallback(
+    throttle(100, updateScrollState),
+    [updateScrollState]
+  );
 
   useEffect(() => {
     if (!element) return undefined;
@@ -59,9 +49,9 @@ export const useHorizontalScrollState = <
     });
     resizeObserver.observe(element);
 
-    const handleScroll = throttle(100, () => updateScrollState());
     element.addEventListener("scroll", handleScroll);
 
+    updateScrollBarHeight();
     updateScrollState();
 
     return () => {
@@ -70,5 +60,5 @@ export const useHorizontalScrollState = <
     };
   }, [element, updateScrollState, updateScrollBarHeight]);
 
-  return { refCallback, state, scrollBarHeight };
+  return { refCallback: setElement, state, scrollBarHeight };
 };
